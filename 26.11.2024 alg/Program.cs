365c38d1e37f418e28e2f3a18ace088c8963526e
Exfor <Exfor@MSI>
2024-11-26 15:08:28 +0300

    Proje dosyası ekle.

---
+++ 26.11.2024 alg/Program.cs
@@ -0,0 +1,44 @@
+﻿List <string> ogrencilist = new List<string>(); 
+
+while (true)
+{
+    int ogrencisayı= 0;
+    Console.WriteLine("girmek istediğiniz öğrencileri yazın.");
+    ogrencilist.Add(Console.ReadLine());
+
+    int kararver = 0;
+    Console.WriteLine("Lütfen eklemek istiyorsanız 1 istemiyorsanız 2ye basın.");
+    kararver= Convert.ToInt32(Console.ReadLine());  
+    if (kararver== 1)
+    {
+        Console.WriteLine("öğrenci eklendi. devam için 1 iptal için 2 " );
+        ogrencisayı++;
+        
+    }
+    else if (kararver == 2) {
+        Console.WriteLine( "işlem tamamlandı toplam öğrenci sayısı" +ogrencisayı );
+        
+    }
+    else
+    {
+        Console.WriteLine(  "geçersiz");
+        
+    }
+
+    Console.WriteLine(" kaç servisiniz var");
+    int servis=Convert.ToInt32(Console.ReadLine()) ;
+    if (servis < ogrencisayı)
+    {
+        Console.WriteLine("servis yetersiz.");
+    }
+    else
+    {
+        Console.WriteLine("Geziye gidebliriz");
+    }
+
+
+
+
+}   
+
+
